a5f5ca9f7aab56b38ff4a5a6803c91eb2c48dd7b
Luiz Hammerli <luizhammerli@gmail.com>
2025-12-29 10:23:16 -0300

    Update TetsProjCIUITestsLaunchTests.swift

--- TetsProjCIUITests/TetsProjCIUITestsLaunchTests.swift
+++ TetsProjCIUITests/TetsProjCIUITestsLaunchTests.swift
@@ -8,10 +8,6 @@
 import XCTest
 
 final class TetsProjCIUITestsLaunchTests: XCTestCase {
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
     override func setUpWithError() throws {
         continueAfterFailure = false
     }
