a36c8f834b5d33dad51aba2e4299f61c4c9a9c31
Luiz Hammerli <luizhammerli@gmail.com>
2025-12-29 10:19:12 -0300

    linting

--- Tets-Proj-CIUITests/Tets_Proj_CIUITestsLaunchTests.swift
+++ Tets-Proj-CIUITests/Tets_Proj_CIUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class Tets_Proj_CIUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
@@ -21,10 +20,6 @@ final class Tets_Proj_CIUITestsLaunchTests: XCTestCase {
     func testLaunch() throws {
         let app = XCUIApplication()
         app.launch()
-
-        // Insert steps here to perform after app launch but before taking a screenshot,
-        // such as logging into a test account or navigating somewhere in the app
-
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
         attachment.lifetime = .keepAlways
